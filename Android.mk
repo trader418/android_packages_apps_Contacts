8d7afa7fdffa6a1a790bc24a688ac432abd5ba44
BlackDragon <blackdragon.fusionteam@gmail.com>
2015-07-24 15:43:06 +0200

    TRDS: Fix aapt removing the darktheme resources.
    Make this package support TRDS resources.
    
    Change-Id: I590ea85ff8080d18b9638c29f3b56418509605bc
    Signed-off-by: BlackDragon <blackdragon.fusionteam@gmail.com>

--- Android.mk
+++ Android.mk
@@ -19,6 +19,8 @@ LOCAL_AAPT_FLAGS := \
     --extra-packages com.android.phone.common \
     --extra-packages android.support.v7.cardview
 
+LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
+
 LOCAL_JAVA_LIBRARIES := telephony-common voip-common
 LOCAL_STATIC_JAVA_LIBRARIES := \
     com.android.vcard \
